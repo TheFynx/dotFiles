0c82e608c4306014f83fb78e30073caa87f9200a
TheFynx <levithegeek@gmail.com>
2014-08-10 18:31:43 -0500

    Updating setup script

--- setup.sh
+++ setup.sh
@@ -6,8 +6,8 @@ HOMEDIR=`pwd`
 
 # Delete Existing Dotfiles
 
-rm -rf /usr/bin/lock
-rm -rf /usr/bin/aerosnap
+#rm -rf /usr/bin/lock
+#rm -rf /usr/bin/aerosnap
 rm -rf $HOMEDIR/.vim
 rm -f $HOMEDIR/.vimrc
 rm -f $HOMEDIR/.bashrc
@@ -17,13 +17,13 @@ rm -f $HOMEDIR/.conkyrc
 rm -rf $HOMEDIR/.config/terminator
 rm -rf $HOMEDIR/.config/tint2
 rm -rf $HOMEDIR/.config/openbox
-rm -rf $HOMEDIR/.config/nitrogen
-rm -r $HOMEDIR/.config/obmenu-generator/config.pl
+#rm -rf $HOMEDIR/.config/nitrogen
+#rm -r $HOMEDIR/.config/obmenu-generator/config.pl
 
 # Create Symlinks
 
-ln -s $DIR/bin/lock /usr/bin/lock
-ln -s $DIR//bin/aerosnap /usr/bin/aerosnap
+#ln -s $DIR/bin/lock /usr/bin/lock
+#ln -s $DIR//bin/aerosnap /usr/bin/aerosnap
 ln -s $DIR/vimrc $HOMEDIR/.vimrc
 ln -s $DIR/vim $HOMEDIR/.vim
 ln -s $DIR/bashrc $HOMEDIR/.bashrc
@@ -33,7 +33,7 @@ ln -s $DIR/conkyrc $HOMEDIR/.conkyrc
 ln -s $DIR/config/terminator $HOMEDIR/.config/terminator
 ln -s $DIR/config/tint2 $HOMEDIR/.config/tint2
 ln -s $DIR/config/openbox $HOMEDIR/.config/openbox
-ln -s $DIR/config/nitrogen $HOMEDIR/.config/nitrogen
+#ln -s $DIR/config/nitrogen $HOMEDIR/.config/nitrogen
 if [ -d $HOMEDIR/.config/obmenu-generator ]; then
     ln -s $DIR/config/obmenu-generator/config.pl $HOMEDIR/.config/obmenu-generator/config.pl
 fi
