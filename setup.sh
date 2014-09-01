822e652f6702ebff0d444cd9f8ff7dc7338930a8
Levi Smith <levithegeek@gmail.com>
2014-09-01 18:11:54 -0500

    Updating tint and openbox setup to remove scrollwheel desktop changes

--- setup.sh
+++ setup.sh
@@ -14,9 +14,11 @@ rm -f $HOMEDIR/.bashrc
 rm -f $HOMEDIR/.bash_profile
 rm -f $HOMEDIR/.profile
 rm -f $HOMEDIR/.conkyrc
+rm -rf $HOMEDIR/.config/conky
 rm -rf $HOMEDIR/.config/terminator
 rm -rf $HOMEDIR/.config/tint2
 rm -rf $HOMEDIR/.config/openbox
+rm -rf $HOMEDIR/.config/xfce4/xfconf/xfce-perchannel-xml
 #rm -rf $HOMEDIR/.config/nitrogen
 #rm -r $HOMEDIR/.config/obmenu-generator/config.pl
 
@@ -30,9 +32,11 @@ ln -s $DIR/bashrc $HOMEDIR/.bashrc
 ln -s $DIR/bash_profile $HOMEDIR/.bash_profile
 ln -s $DIR/profile $HOMEDIR/.profile
 ln -s $DIR/conkyrc $HOMEDIR/.conkyrc
+ln -s $DIR/config/conky $HOMEDIR/.config/conky
 ln -s $DIR/config/terminator $HOMEDIR/.config/terminator
 ln -s $DIR/config/tint2 $HOMEDIR/.config/tint2
 ln -s $DIR/config/openbox $HOMEDIR/.config/openbox
+ln -s $DIR/config/xfce4/xfconf/xfce-perchannel-xml $HOMEDIR/.config/xfce4/xfconf/xfce-perchannel-xml
 #ln -s $DIR/config/nitrogen $HOMEDIR/.config/nitrogen
 if [ -d $HOMEDIR/.config/obmenu-generator ]; then
     ln -s $DIR/config/obmenu-generator/config.pl $HOMEDIR/.config/obmenu-generator/config.pl
