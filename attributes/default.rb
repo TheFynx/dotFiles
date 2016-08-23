2894466d22b8e3c4842e48eeea9a867540aa6be6
Levi Smith <levi@fynx.me>
2016-08-22 22:33:17 -0500

    Adding new branch to re-work dotfiles completely

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,47 +1,14 @@
-case profile
-when "work"
-    case platform
-    when "mac_os_x"
-        default[:dotfiles] = {
-            user: "levi.smith",
-            group: "levi.smith",
-            home_path: "/Users/levi.smith"
-        }
-    else
-        default[:dotfiles] = {
-           user: "levi.smith",
-           group: "levi.smith",
-           home_path: "/home/levi.smith"
-        }
-   end
-when "personal"
-    case platform
-    when "mac_os_x"
-        default[:dotfiles] = {
-            user: "levi",
-            group: "staff",
-            home_path: "/Users/levi"
-        }
-    else
-        default[:dotfiles] = {
-           user: "levi",
-           group: "levi",
-           home_path: "/home/levi"
-        }
-    end
+case platform
+when "mac_os_x"
+  default[:dotfiles] = {
+    user: "levi",
+    group: "staff",
+    home_path: "/Users/levi"
+  }
 else
-    case platform
-    when "mac_os_x"
-        default[:dotfiles] = {
-            user: "vagrant",
-            group: "vagrant",
-            home_path: "/Users/vagrant"
-        }
-    else
-        default[:dotfiles] = {
-           user: "vagrant",
-           group: "vagrant",
-           home_path: "/home/vagrant"
-        }
-    end
+  default[:dotfiles] = {
+    user: "levi",
+    group: "levi",
+    home_path: "/home/levi"
+  }
 end
