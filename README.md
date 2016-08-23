c5b2456e2b9613dd81b97fb066173dc7eaba4142
Levi Smith <levi@fynx.me>
2016-08-22 22:52:11 -0500

    adding fonts, updating readme, updating install script

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-# dotfiles
+# The Fynx's dotFiles
 
-This is a [Chef](http://www.opscode.com/) cookbook to add my dotfiles to a machine. It also contains a script and some configuration files to allow you to provision the machine you're currently on with the default recipe.
+## Revamped Edition
 
-To set up your own workstation:
+## Install
 
-1. Clone or simply download the repository to your computer.
-1. (Optional) Edit `node.json` to override any of the default attributes. The default attributes assume your user is "Levi".
-1. Run `install.sh`.
+## Options/Controls
+
+## Thanks
--- install.sh
+++ install.sh
@@ -1,29 +1,57 @@
 #!/bin/bash
-###
-#Usage
-# ./install.sh personal (for personal setup)
-# ./install.sh work (for work setup)
-# ./install.sh (for vagrant setup)
-###
-# Install Chef
-if [ ! -f /usr/bin/chef-solo ]; then
-  curl -L https://www.opscode.com/chef/install.sh -o install_chef.sh
-  sudo bash install_chef.sh
-  rm install_chef.sh
+
+#!/usr/bin/env bash
+
+set -o nounset -o pipefail
+
+bootstrapCookbook='bootstrap-osx'
+tempInstallDir="mktemp -d -t ${bootstapCookbook}"
+
+# create Berksfile so that we can install the correct cookbook dependencies
+cat > "${tempInstallDir}/Berksfile" <<EOF
+source 'https://supermarket.chef.io'
+cookbook '$bootstrapCookbook';
+cookbook '$dotfiles';
+EOF
+
+# create client.rb file so that Chef client can find its dependant cookbooks
+cat > "${tempInstallDir}/client.rb" <<EOF
+cookbook_path File.join(Dir.pwd, 'berks-cookbooks')
+EOF
+
+cat <<EOF
+** Installing ChefDK
+EOF
+
+if [[ "$INSTALL_CHEFDK" -eq 1 ]]
+then
+  echo "Installing ChefDK ${CHEFDK_TARGET_VERSION}"
+  curl --silent --show-error https://omnitruck.chef.io/install.sh | \
+    sudo -E bash -s -- -c stable -P chefdk || prompt_continue
 fi
 
-# Copy over the cookbooks
-sudo mkdir -p /var/chef/cookbooks
-sudo rm -rf /var/chef/cookbooks/dotfiles
-sudo cp -R . /var/chef/cookbooks/dotfiles
+echo "Downloading cookbook dependencies with Berkshelf"
+cd "$tempInstallDir" || prompt_continue
+chef exec berks vendor || prompt_continue
 
-# Run Chef
-if [ "${1}" == 'work' ]; then
-    sudo /usr/bin/chef-solo -c solo.rb -j work.json
-elif [ "${1}" == 'personal' ]; then
-    sudo /usr/bin/chef-solo -c solo.rb -j personal.json
-else
-    sudo /usr/bin/chef-solo -c solo.rb -j default.json
+# Pass optional attributes to chef-client
+# This is a temporary interface and will change in 2.0 when we support named parameters (Issue #74)
+if [ -n "${BOOTSTRAP_JSON_ATTRIBUTES:-}" ]
+then
+  attributeParameter=" --json-attributes $CHEFDK_BOOTSTRAP_JSON_ATTRIBUTES"
 fi
 
-vim +BundleClean! +BundleInstall! +qall!
+echo "Running chef-client (installed by ChefDK) to bootstrap this machine"
+sudo -E chef-client -z -l error -c "${tempInstallDir}/client.rb" -o "$bootstrapCookbook" ${attributeParameter:-} || prompt_continue
+
+# cleanup
+cd - || prompt_continue
+sudo rm -rf "$tempInstallDir"
+
+# End message to direct Mac users to last step in set up
+cat <<EOF
+You're almost done!!! You just need to edit your shell startup script to set up
+chefdk environment variables for each login. See this page:
+https://github.com/chef/chef-dk#using-chefdk-as-your-primary-development-environment
+EOF
+
