5ef86424192a26b70d0aebdf30e3b7c36d075599
Levi Smith <levi@fynx.me>
2016-08-28 21:54:55 -0500

    cleaning up functions and profiles, adding attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,23 +1,27 @@
 case platform
 when "mac_os_x"
-  default["dotfiles"].tap do |profile|
+  default["dotfiles"]["profile"].tap do |profile|
     profile["group"] = "levi",
     profile["group"] = "staff",
     profile["home"] = "/Users//#{node["dotfiles"]["profile"]["user"]}"
   end
 
 when "windows"
-  default["dotfiles"].tap do |profile|
+  default["dotfiles"]["profile"].tap do |profile|
     profile["group"] = "levi",
     profile["group"] = "levi",
     profile["home"] = "C:/users/#{node["dotfiles"]["profile"]["user"]}"
   end
 else
-  default["dotfiles"].tap do |profile|
+  default["dotfiles"]["profile"].tap do |profile|
     profile["group"] = "levi",
     profile["group"] = "levi",
     profile["home"] = "/home//#{node["dotfiles"]["profile"]["user"]}"
   end
 end
 
+
+default["dotfiles"]["docker"].tap do |docker|
+  docker["user"] = "thefynx"
+end
 
