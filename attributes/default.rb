6b02125d17a40e1267113842b31506ae3aa8b359
Levi Smith <levi@fynx.me>
2016-09-07 11:37:52 -0500

    fixing attributes, again

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,27 +1,17 @@
 case platform
 when "mac_os_x"
-  default["dotfiles"]["profile"].tap do |profile|
-    profile["user"] = "levi",
-    profile["group"] = "staff",
-    profile["home"] = "/Users/#{node["dotfiles"]["profile"]["user"]}"
-  end
-
+    default["dotfiles"]["profile"]["user"] = "levi"
+    default["dotfiles"]["profile"]["group"] = "staff"
+    default["dotfiles"]["profile"]["home"] = "/Users/#{node["dotfiles"]["profile"]["user"]}"
 when "windows"
-  default["dotfiles"]["profile"].tap do |profile|
-    profile["user"] = "levi",
-    profile["group"] = "levi",
-    profile["home"] = "C:/users/#{node["dotfiles"]["profile"]["user"]}"
-  end
+    default["dotfiles"]["profile"]["user"] = "levi"
+    default["dotfiles"]["profile"]["group"] = "levi"
+    default["dotfiles"]["profile"]["home"] = "C:/users/#{node["dotfiles"]["profile"]["user"]}"
 else
-  default["dotfiles"]["profile"].tap do |profile|
-    profile["user"] = "levi",
-    profile["group"] = "levi",
-    profile["home"] = "/home/#{node["dotfiles"]["profile"]["user"]}"
-  end
+    default["dotfiles"]["profile"]["user"] = "levi"
+    default["dotfiles"]["profile"]["group"] = "levi"
+    default["dotfiles"]["profile"]["home"] = "/home/#{node["dotfiles"]["profile"]["user"]}"
 end
 
 
-default["dotfiles"]["docker"].tap do |docker|
-  docker["user"] = "thefynx"
-end
-
+["dotfiles"]["docker"]["user"] = "thefynx"
