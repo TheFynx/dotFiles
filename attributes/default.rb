72b6ae4185e69093d3f073eac3fe46757c6c891f
Levi Smith <levi@fynx.me>
2016-09-07 11:05:33 -0500

    fixing broken attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,20 +1,20 @@
 case platform
 when "mac_os_x"
   default["dotfiles"]["profile"].tap do |profile|
-    profile["group"] = "levi",
+    profile["user"] = "levi",
     profile["group"] = "staff",
     profile["home"] = "/Users/#{node["dotfiles"]["profile"]["user"]}"
   end
 
 when "windows"
   default["dotfiles"]["profile"].tap do |profile|
-    profile["group"] = "levi",
+    profile["user"] = "levi",
     profile["group"] = "levi",
     profile["home"] = "C:/users/#{node["dotfiles"]["profile"]["user"]}"
   end
 else
   default["dotfiles"]["profile"].tap do |profile|
-    profile["group"] = "levi",
+    profile["user"] = "levi",
     profile["group"] = "levi",
     profile["home"] = "/home/#{node["dotfiles"]["profile"]["user"]}"
   end
