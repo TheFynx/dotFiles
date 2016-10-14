5c3e5640b6389a2bf3407226698d95e30acffa11
Levi Smith <levi@fynx.me>
2016-10-14 16:05:13 -0500

    well that was stupid

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,15 +2,15 @@ case platform
 when "mac_os_x"
     default["dotfiles"]["profile"]["user"] = "levi"
     default["dotfiles"]["profile"]["group"] = "staff"
-    default["dotfiles"]["profile"]["home"] = "/Users/(node["dotfiles"]["profile"]["user"])"
+    default["dotfiles"]["profile"]["home"] = "/Users/#{node["dotfiles"]["profile"]["user"]}"
 when "windows"
     default["dotfiles"]["profile"]["user"] = "levi"
     default["dotfiles"]["profile"]["group"] = "levi"
-    default["dotfiles"]["profile"]["home"] = "C:/users/(node["dotfiles"]["profile"]["user"])"
+    default["dotfiles"]["profile"]["home"] = "C:/users/#{node["dotfiles"]["profile"]["user"]}"
 else
     default["dotfiles"]["profile"]["user"] = "levi"
     default["dotfiles"]["profile"]["group"] = "levi"
-    default["dotfiles"]["profile"]["home"] = "/home/(node["dotfiles"]["profile"]["user"])"
+    default["dotfiles"]["profile"]["home"] = "/home/#{node["dotfiles"]["profile"]["user"]}"
 end
 
 
